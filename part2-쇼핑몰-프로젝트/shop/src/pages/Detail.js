6d150b44a3bac4da7955a23473fcecf87136f8e6
chanki <coldair426@gmail.com>
2023-02-12 02:34:46 +0900

    useEffect

--- part2-쇼핑몰-프로젝트/shop/src/pages/Detail.js
+++ part2-쇼핑몰-프로젝트/shop/src/pages/Detail.js
@@ -2,16 +2,32 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Detail(props) {
+  let [sale, setSale] = useState(1);
   let [count, setCount] = useState(0);
-
   let { id } = useParams();
   let pickedShoes = props.shoes.find((obj) => obj.id === +id);
+  let [warn, setWarn] = useState(false);
+  let [userInput, setUserInput] = useState('');
 
-  setTimeout(() => {}, 2000);
+  console.log(Number(userInput));
+
+  useEffect(() => {
+    let a = setTimeout(() => setSale(0), 2000);
+    return () => {
+      clearTimeout(a);
+    };
+  }, []);
+
+  useEffect(() => {
+    Number(userInput) || Number(userInput) === 0 ? setWarn(false) : setWarn(true);
+    return () => {
+      setWarn(0);
+    };
+  }, [userInput]);
 
   return (
     <div className='container'>
-      <div className='alert alert=warning'>2초이내 구매시 할인</div>
+      {sale ? <div className='alert alert-warning'>2초이내 구매시 할인</div> : null}
       {count}
       <button
         onClick={() => {
@@ -24,6 +40,12 @@ function Detail(props) {
           <img src={pickedShoes.img} width='100%' />
         </div>
         <div className='col-md-6'>
+          {warn === false ? null : <div className='alert alert-warning'>경고:숫자만입력하세요</div>}
+          <input
+            onChange={(e) => {
+              setUserInput(e.target.value);
+            }}
+          />
           <h4 className='pt-5'>{pickedShoes.title}</h4>
           <p>{pickedShoes.content}</p>
           <p>{pickedShoes.price}</p>
