83daf655902458bea87be9657fba8c7b240db8ab
chanki <coldair426@gmail.com>
2023-02-15 23:40:20 +0900

    lazyImport

--- part2-쇼핑몰-프로젝트/shop/src/App.js
+++ part2-쇼핑몰-프로젝트/shop/src/App.js
@@ -1,12 +1,13 @@
-import { createContext, useEffect, useState } from 'react';
+import { lazy, Suspense, createContext, useEffect, useState } from 'react';
 import { Row, Col, Navbar, Nav, Container } from 'react-bootstrap';
 import './App.css';
 import data from './data.js';
-import Detail from './pages/Detail.js';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import axios from 'axios';
-import Cart from './pages/Cart.js';
 import { useQuery } from 'react-query';
+
+const Detail = lazy(() => import('./pages/Detail.js'));
+const Cart = lazy(() => import('./pages/Cart.js'));
 
 let Context1 = createContext();
 
@@ -74,68 +75,69 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <>
-              <div className='main-bg'></div>
-              <Row>
-                {shoes.map((_, i) => {
-                  return <Item shoes={shoes[i]} index={i} />;
-                })}
-              </Row>
-              <button
-                onClick={() => {
-                  setLoading(true);
-                  setMoreButton((moreButton += 1));
-                  if (moreButton === 1) {
-                    axios
-                      .get('https://codingapple1.github.io/shop/data2.json')
-                      .then((data) => {
-                        const arr = [...shoes, ...data.data];
-                        setShoes(arr);
-                        setLoading(false);
-                      })
-                      .catch(() => {
-                        console.log('실패함ㅅㄱ');
-                        setLoading(false);
-                      });
-                  } else if (moreButton === 2) {
-                    axios
-                      .get('https://codingapple1.github.io/shop/data3.json')
-                      .then((data) => {
-                        const arr = [...shoes, ...data.data];
-                        setShoes(arr);
-                        setLoading(false);
-                      })
-                      .catch(() => {
-                        console.log('실패함ㅅㄱ');
-                        setLoading(false);
-                      });
-                  } else {
-                    alert('더 이상 상품이 없어요~~');
-                    setLoading(false);
-                  }
-                }}>
-                더보기
-              </button>
-              {loading ? <span>로딩중~</span> : null}
-            </>
-          }></Route>
-        <Route path='/detail/:id' element={<Detail shoes={shoes} />}></Route>
-        <Route path='/about' element={<About />}>
-          <Route path='member' element={<div>멤버임</div>} />
-          <Route path='location' element={<div>위치정보임</div>} />
-        </Route>
-        <Route path='/event' element={<Event></Event>}>
-          <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>} />
-          <Route path='two' element={<div>생일기념 쿠폰받기</div>} />
-        </Route>
-        <Route path='/cart' element={<Cart />} />
-        <Route path='*' element={<div>없는페이지요</div>}></Route>
-      </Routes>
+      <Suspense fallback={<div>로딩중</div>}>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <>
+                <div className='main-bg'></div>
+                <Row>
+                  {shoes.map((_, i) => {
+                    return <Item shoes={shoes[i]} index={i} />;
+                  })}
+                </Row>
+                <button
+                  onClick={() => {
+                    setLoading(true);
+                    setMoreButton((moreButton += 1));
+                    if (moreButton === 1) {
+                      axios
+                        .get('https://codingapple1.github.io/shop/data2.json')
+                        .then((data) => {
+                          const arr = [...shoes, ...data.data];
+                          setShoes(arr);
+                          setLoading(false);
+                        })
+                        .catch(() => {
+                          console.log('실패함ㅅㄱ');
+                          setLoading(false);
+                        });
+                    } else if (moreButton === 2) {
+                      axios
+                        .get('https://codingapple1.github.io/shop/data3.json')
+                        .then((data) => {
+                          const arr = [...shoes, ...data.data];
+                          setShoes(arr);
+                          setLoading(false);
+                        })
+                        .catch(() => {
+                          console.log('실패함ㅅㄱ');
+                          setLoading(false);
+                        });
+                    } else {
+                      alert('더 이상 상품이 없어요~~');
+                      setLoading(false);
+                    }
+                  }}>
+                  더보기
+                </button>
+                {loading ? <span>로딩중~</span> : null}
+              </>
+            }></Route>
+          <Route path='/detail/:id' element={<Detail shoes={shoes} />}></Route>
+          <Route path='/about' element={<About />}>
+            <Route path='member' element={<div>멤버임</div>} />
+            <Route path='location' element={<div>위치정보임</div>} />
+          </Route>
+          <Route path='/event' element={<Event></Event>}>
+            <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>} />
+            <Route path='two' element={<div>생일기념 쿠폰받기</div>} />
+          </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='*' element={<div>없는페이지요</div>}></Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
